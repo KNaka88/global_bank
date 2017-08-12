1e3f7f2ced01e54081ea34422b6c1066cdf9b9d2
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-08-12 16:55:55 -0700

    add files

---
+++ public/staff/subjects/show.php
@@ -0,0 +1,13 @@
+<?php require_once('../../../private/initialize.php') ?>
+
+<?php
+
+$id = isset($_GET['id']) ? $_GET['id'] : '1';
+
+echo h($id);
+
+?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a> <br>
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a> <br>
+<a href="show.php?name=<?php echo u('!#*?'); ?>">Link</a><br>
